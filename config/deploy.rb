994ab6a6546abcfcadaaf1c690cfc021ac0a83d3
Alex Duller <alexduller@gmail.com>
2009-07-11 11:53:10 +0100

    Setup for moretolife

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,34 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "moretolife"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
+set :deploy_to,  "/home/alexd/public_html/#{application}"
+set :deploy_via, :remote_cache
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, :git
+set :repository,  "ssh://git@174.143.244.92:30000/home/git/moretolife.git"
+set :branch, "master"
+set :user, 'alexd'
+ssh_options[:forward_agent] = true
+set :port, 30000
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "moretolife08.org.uk"
+role :web, "moretolife08.org.uk"
+role :db,  "moretolife08.org.uk", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true }  do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
