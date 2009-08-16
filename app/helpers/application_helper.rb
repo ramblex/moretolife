c9a122963300284c14f6638c2efd019ade1a05d0
Alex Duller <alexduller@gmail.com>
2009-08-16 11:31:15 +0100

    Added handling for events spanning multiple days

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def date_format(date_begin, date_end)
-    time = date_begin.strftime("%l:%M %p").downcase
-    date = date_begin.strftime("%A %e %B")
-    # Make the assumption that no events start at midnight
-    if time.eql?("12:00 am")
-        date
+    time = date_begin.strftime(" %l:%M %p").downcase
+    # Don't show the time if it's set to midnight - Assume no events start
+    # at midnight unless they are an 'all day' event.
+    if time == " 12:00 am"
+      time = ""
+    end
+
+    # Handle events spanning multiple days
+    if date_end > date_begin
+      if date_begin.month == date_end.month
+        start_day = date_begin.strftime("%A %e")
+      else
+        start_day = date_begin.strftime("%A %e %B")
+      end
+      end_day = date_end.strftime("%e %B")
+      "#{start_day}-#{end_day}#{time}"
     else
-      "#{date}, #{time}"
+      date = date_begin.strftime("%A %e %B")
+      "#{date}#{time}"
     end
   end
-  
+
   def title(page_title)
     content_for(:title) { page_title }
   end
