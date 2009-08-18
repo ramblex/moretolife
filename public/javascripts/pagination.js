50237ca25813a0900233e89861b8eaa2b8b01d3b
Alex Duller <alexduller@gmail.com>
2009-08-18 11:17:52 +0100

    Added support for AJAX history and bookmarks

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,17 @@
 $(function() {
     $(".pagination a").live("click", function() {
+        $.setFragment({ "page" : $.queryString(this.href).page })
         $(".pagination").html("<img src='/images/ajax-loader.gif' />");
-        $.get(this.href, null, null, "script");
         return false;
       });
+
+    $.fragmentChange(true);
+    $(document).bind("fragmentChange.page", function() {
+        $.getScript($.queryString(document.location.href, 
+                                  { "page" : $.fragment().page }));
+      });
+
+    if ($.fragment().page) {
+      $(document).trigger("fragmentChange.page");
+    }
   });
