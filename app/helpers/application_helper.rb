3b9534be19090f84905084644bf20b1e81a511f4
Alex Duller <alexduller@gmail.com>
2010-07-04 15:31:54 +0100

    Tidied up 'current' link in left nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,24 @@ module ApplicationHelper
   def javascript(file)
     content_for(:head) { javascript_include_tag file }
   end
+
+  class CurrentPageDecorator
+    def initialize(helper,options)
+       @helper = helper
+       @html_class = options[:class] || 'active'
+    end
+
+    def link_to(*args,&blk)
+      name = args.first
+      options = args.second || { }
+      html_options = args.third || { }
+      html_options[:class] = @html_class if @helper.current_page?(options)
+      @helper.link_to(name,options,html_options,blk)
+    end
+  end
+
+  def highlight_current_link(options = { },&blk)
+    raise ArgumentError unless block_given?
+    yield CurrentPageDecorator.new(self,options)
+  end
 end
